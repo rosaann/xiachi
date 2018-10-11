577732bf7661665fe279a04c9d44d77db8b5a76a
zl <zengl65e@visionvera.com>
2018-10-11 17:56:03 +0800

    d

---
+++ run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Run CIFAR10 experiment on ganomaly
+
+declare -a arr=("plane" "car" "bird" "cat" "deer" "dog" "frog" "horse" "ship" "truck" )
+for m in {0..2}
+do
+    echo "Manual Seed: $m"
+    for i in "${arr[@]}";
+    do
+        echo "Running CIFAR. Anomaly Class: $i "
+        python train.py --dataset cifar10 --isize 32 --niter 15 --anomaly_class $i --manualseed $m --display --display_port 8888
+    done
+done
+exit 0
